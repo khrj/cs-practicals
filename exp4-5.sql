81fc2182e60e8e855b46a41b6bffac98451d25cb
Khushraj Rathod <khushraj.rathod@gmail.com>
2023-09-26 14:45:31 +0530

    Create exp4-5.sql

---
+++ exp4-5.sql
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="1234",
+  database="cspract"
+)
+
+cursor = mydb.cursor()
+
+cursor.execute("SELECT * FROM student;")
+rows = cursor.fetchall()
+print(rows)
+
+cursor.execute("DELETE FROM student WHERE stname='abc';")
